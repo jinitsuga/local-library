a16270988f07f0a58df230ca6ea54372f13a7408
agu <iba.agustin@gmail.com>
2023-01-30 17:22:30 -0300

    connecting to mongodb

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+
+mongoose.set("strictQuery", false);
+
+const mongoDB =
+  "mongodb+srv://jinitsuga:qweasd356@talentscluster.o0zdqp1.mongodb.net/test";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
