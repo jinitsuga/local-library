ea7ddf739ccac2df8fca55704fe38600955a90a1
agu <iba.agustin@gmail.com>
2023-02-11 15:14:58 -0300

    started catalog file, added book routes

---
+++ routes/catalog.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+// controller modules
+
+const author_controller = require("../controllers/authorController");
+const book_controller = require("../controllers/bookController");
+const book_instance_controller = require("../controllers/bookInstanceController");
+const genre_controller = require("../controllers/genreController");
+
+// index route
+
+router.get("/", book_controller.index);
+
+//  --- Book routes ---
+
+// creation
+router.get("/book/create", book_controller.book_create_get);
+router.post("book/create", book_controller.book_create_post);
+
+//deletion
+router.get("/book/:id/delete", book_controller.book_delete_get);
+router.post("/book/:id/delete", book_controller.book_delete_post);
+
+// update
+router.get("/book/:id/update", book_controller.book_update_get);
+router.post("/book/:id/update", book_controller.book_update_post);
+
+// book details and list of books
+router.get("/book/:id", book_controller.book_details);
+
+router.get("/books", book_controller.book_list);
