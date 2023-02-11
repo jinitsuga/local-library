7c7315b417b2830c0290be8251c014a3a7d7efb0
agu <iba.agustin@gmail.com>
2023-02-11 14:32:53 -0300

    added genre controllers

---
+++ controllers/genreController.js
@@ -0,0 +1,41 @@
+const Genre = require("../models/genre");
+
+// Shows list of all genres
+exports.genre_list = (req, res) => {
+  res.send("not implemented: genre list");
+};
+
+// Display specific details of a genre
+exports.genre_details = (req, res) => {
+  res.send("not implemented: genre details");
+};
+
+// Show genre creation form
+exports.genre_create_get = (req, res) => {
+  res.send("not implemented: genre creation form");
+};
+
+// Create the genre on the DB from creation form (post)
+exports.genre_create_post = (req, res) => {
+  res.send("not implemented: genre creation on db");
+};
+
+// show genre delete form
+exports.genre_delete_get = (req, res) => {
+  res.send("not implemented: deletion form");
+};
+
+// send deletion post to DB
+exports.genre_delete_post = (req, res) => {
+  res.send("not implemented: post deletion to db");
+};
+
+// show update genre form
+exports.genre_update_get = (req, res) => {
+  res.send("not implemented: update genre form");
+};
+
+// post update genre to db
+exports.genre_update_post = (req, res) => {
+  res.send("not implemented: post update to db");
+};
