19fea3c477ab6d15d8e05c43f82b26cf6201bc39
agu <iba.agustin@gmail.com>
2023-02-11 15:21:02 -0300

    added author routes

--- routes/catalog.js
+++ routes/catalog.js
@@ -38,8 +38,14 @@ router.get("/author/create", author_controller.author_create_get);
 router.post("/author/create", author_controller.author_create_post);
 
 // deletion
-router.get("/author/:id/delete", author_controller.author_dele);
+router.get("/author/:id/delete", author_controller.author_delete_get);
+router.post("/author/:id/delete", author_controller.author_delete_post);
 
 // update
+router.get("/author/:id/update", author_controller.author_update_get);
+router.post("/author/:id/update", author_controller.author_update_post);
 
 // details and list of authors
+router.get("/author/:id", author_controller.author_details);
+
+router.get("/authors", author_controller.author_list);
